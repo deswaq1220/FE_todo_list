fa65405ddb8a59c1006a15cd27beafc6dccdc07d
deswaq1220 <deswaq789@naver.com>
2025-02-28 02:18:49 +0900

    refactor:라우터정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { RecoilRoot } from 'recoil';
-import AddTodo from './components/AddTodo';
-import SearchInput from './components/SearchInput';
-import TodoList from './components/TodoList';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './pages/mainPage';
 
 function App() {
   return (
     <RecoilRoot>
-      <div className='flex h-screen w-full justify-center bg-gray-50'>
-        <div className='w-[700px] p-5'>
-          <h1 className='mb-2 text-2xl'>투두네이션</h1>
-          <div className='flex flex-col gap-y-3'>
-            <SearchInput />
-            <AddTodo />
-            <TodoList />
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+      </Routes>
     </RecoilRoot>
   );
 }
